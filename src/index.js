deda10b5c32c3cf77fea5b59bd74dd39ee1cf7c0
Khristian Brooks <kh.brooks02@gmail.com>
2018-04-28 13:24:16 -0400

    Added firebase to project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+// initialize firebase
+var config = {
+	apiKey: 'AIzaSyCxiahlpRrCWYpUJCQ5sm7V3GfIw6FOqXc',
+	authDomain: 'deli-hero.firebaseapp.com',
+	databaseURL: 'https://deli-hero.firebaseio.com',
+	projectId: 'deli-hero',
+	storageBucket: '',
+	messagingSenderId: '572405583565',
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
