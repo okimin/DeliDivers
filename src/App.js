7675879b47953b4a9db861547ac4952b264bd98c
Khristian Brooks <kh.brooks02@gmail.com>
2018-04-29 11:02:24 -0400

    added a background image to the home page

--- src/App.js
+++ src/App.js
@@ -31,12 +31,11 @@ const Home = () => (
 	<div className="App">
 		<header className="App-header">
 			<img src={logo} className="App-logo" alt="logo" />
-			<h1 className="App-title">Welcome to React</h1>
+			<h1 className="App-caption">Never Eat A Bad Sandwich Again...</h1>
 		</header>
 		<p className="App-intro">
 			To get started, edit <code>src/App.js</code> and save to reload.
 		</p>
-		<p> Hey! </p>
 	</div>
 );
 
